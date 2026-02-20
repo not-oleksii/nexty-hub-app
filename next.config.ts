280b529611c6e35386b0d07fae4fea9f26748a48
d1r <mr.battery.ua@gmail.com>
2026-02-20 20:38:31 +0200

    fix(next-config): remove temp statement

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' },
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
 };
 
