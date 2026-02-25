9e8b4fa6ede9a6243c87f7fe3c75ffb0b3f6bb22
d1r <mr.battery.ua@gmail.com>
2026-02-25 18:38:55 +0200

    fix(imports): update imports

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import * as React from 'react';
+
 import { Drawer as DrawerPrimitive } from 'vaul';
 
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils/common';
 
 function Drawer({
   ...props
@@ -123,13 +124,13 @@ function DrawerDescription({
 
 export {
   Drawer,
-  DrawerPortal,
-  DrawerOverlay,
-  DrawerTrigger,
   DrawerClose,
   DrawerContent,
-  DrawerHeader,
-  DrawerFooter,
-  DrawerTitle,
   DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerTitle,
+  DrawerTrigger,
 };
--- src/components/ui/empty.tsx
+++ src/components/ui/empty.tsx
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils/common';
 
 function Empty({ className, ...props }: React.ComponentProps<'div'>) {
   return (
@@ -96,9 +96,9 @@ function EmptyContent({ className, ...props }: React.ComponentProps<'div'>) {
 
 export {
   Empty,
-  EmptyHeader,
-  EmptyTitle,
-  EmptyDescription,
   EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
   EmptyMedia,
+  EmptyTitle,
 };
